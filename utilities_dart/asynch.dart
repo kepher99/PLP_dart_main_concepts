8ff321a2a27fdd565c0f1ec16a723d3dafbbc738
plp_academy <kephero99@gmail.com>
2024-04-04 14:19:04 +0300

    asynvhronous operation

--- utilities_dart/asynch.dart
+++ utilities_dart/asynch.dart
@@ -1,13 +1,16 @@
 import 'dart:async';
 
 void main()async{
+  // Start the asynchronous operation
   print("Fetch weather data....");
   String weather = await fetchWeatherData();
+  // This line will be executed only after fetchUserData() completes
   print("weather data: $weather");
 }
 
 //future function to get weather data
 Future <String> fetchWeatherData(){
+  // Simulate fetching user data asynchronously
   return Future.delayed(Duration(seconds: 3), (){
     return "sunny";
   }); 
